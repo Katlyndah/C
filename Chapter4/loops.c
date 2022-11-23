15e2e41fe466ee2644db583ce251dc1f0547a440
Lyndah Katusiime <Lyndahkats@gmail.com>
2022-11-23 21:34:32 +0300

    Loops in C programming

---
+++ Chapter4/loops.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    //while loop
+    int i = 0;
+    while (i<34)
+    {
+        printf("The value of i is %d\n", i);
+        i++; //or i +=1; or i= i + 1;
+    }
+    
+    return 0;
+}
